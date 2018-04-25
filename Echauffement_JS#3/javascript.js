af4212b64a112d5bb200294d4e096e32f94fc61d
Omniprezz <geoffrey.frioli@gmail.com>
2018-04-25 15:06:02 +0200

    Finished

--- Echauffement_JS#3/javascript.js
+++ Echauffement_JS#3/javascript.js
@@ -1,15 +1,34 @@
-function Log() {
-
-var login = prompt("Quel est ton adresse mail ?", "lea@gmail.com");
-var mdp = prompt("Mot de passe:");
-
-
-if(login === "lea@gmail.com"){
-	return(mdp);
+function Login(){
+  var adress = prompt ("Entrer votre adresse mail:");
+  var n = adress.indexOf("@");
+    
+  if(adress.length >4 && n !== -1){
+  alert ("ok")
+  return adress;
 }else{
-	return("Non valide");
+  alert("Adresse non valide")
+  Login();
+}
 }
 
-};
+function mdp(){
+  var motDePass = prompt ("Entrer votre mot de passe:");
+  return motDePass;
+}
 
-alert(Log());
+
+var newAdress = Login();
+if (newAdress === "lea@gmail.com"){
+  var newMdp = mdp();
+  if(newMdp === "12345"){
+    alert("salut lea");
+  }else{
+    mdp();
+  }
+  
+}else{
+  Login()
+}
+
+console.log (newAdress);
+console.log (newMdp);
