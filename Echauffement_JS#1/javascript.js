1aca8abe3c7b28a4c85d5ffb8bc26fbd5e03bae4
Omniprezz <geoffrey.frioli@gmail.com>
2018-04-25 11:36:43 +0200

    Finished

--- Echauffement_JS#1/javascript.js
+++ Echauffement_JS#1/javascript.js
@@ -4,9 +4,10 @@ function nom() { //ici ma fonction
 var id = prompt ("Comment tu t'appelles?"); //ici ma variable
 
 if(id.length <10 && id.length >1){ //ici ma condition (si id entre 1 et 10 lettres return bonjour sinon retour a la function nom())
-  return("Bonjour, " + id + "!");
+ 	return("Bonjour, " + id + "!");
 }else{
-  return(nom());
+	alert("Nom non valide");
+ 	return(nom());
 }
 }
 
