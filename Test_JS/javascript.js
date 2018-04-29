37726c49285bb3c45b439cf38a686d0e75404ad7
Omniprezz <geoffrey.frioli@gmail.com>
2018-04-29 14:23:47 +0200

    I love JS

--- Test_JS/javascript.js
+++ Test_JS/javascript.js
@@ -1,3 +1,6 @@
+                                
+                                // Les Boucles
+
 var nbr1 = parseInt (prompt("Veuillez entrer votre premier nombre:"));
 while(isNaN(nbr1)){
   nbr1 = parseInt(prompt("Je n'ai pas bien compris... Vueillez entrer votre premier nombre:"));
@@ -9,7 +12,6 @@ while(isNaN(nbr1)){
 }
 
 var resultat = nbr1 + nbr2;
-
 
 
 
@@ -26,3 +28,95 @@ var resultat = nbr1 + nbr2;
 
 
 alert (nbr1 + " + " + nbr2 + " = " + resultat);
+
+
+
+
+for(var i = 0; i <= 10; i++) {
+	console.log(i);
+}
+
+
+
+                                   // Les Tableaux
+
+
+var pseudo = "Omni";
+var tableau = ["PHP", "C++", "Ruby", 15, pseudo];
+var langageProg = tableau[3];
+
+console.log(tableau);
+console.log(tableau[1]);
+tableau[4] = "Python";
+console.log(tableau);
+tableau.pop();// supprime derniére valeur
+tableau.shift();// supprime premiére valeur
+tableau.push(["HTML", "CSS"]);
+console.log(tableau);
+
+
+
+var langagesProg = [];
+
+while(true) {
+	var promptText = prompt("Entrer un langage de programmation");
+	if(promptText != " ") {
+		langagesProg.push(promptText);
+	} else {
+		break;
+	}
+}
+
+console.log(langagesProg);
+
+
+var langageProg = ["C++", "C#", "Ruby", "Python"];
+
+alert(langageProg[0]);
+alert(langageProg.indexOf("Ruby"));
+
+var promptText = prompt("Entrer le nom d'un langage de prog");
+if(langageProg.indexOf(promptText) != -1) {
+	alert("Ton langage existe");
+} else {
+	alert("Ton langage n'existe pas");
+}
+
+
+langageProg.splice(1, 2, "PHP", "JS");
+console.log(langageProg);
+
+console.log(langageProg.length);
+
+for(var i = 0; i < langageProg.length; i++) {
+	alert(langageProg[i]);
+}
+
+
+                            // Fonctions
+
+
+function nomDeLaFonction(param1, param1, param3) {
+
+}
+
+
+function ditBonjour() {
+	alert("Bonjour!");
+}
+
+ditBonjour();
+
+
+function additionne(nbr1, nbr2) {
+	alert(nbr1 + nbr2);
+}
+
+additionne(3, 2);
+
+function additionne(nbr1, nbr2) {
+	alert(nbr1 + nbr2);
+}
+
+var prompt1 = prompt("Votre nombre 1");
+var prompt2 = prompt("Votre nombre 2");
