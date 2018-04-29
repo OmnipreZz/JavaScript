13dbff70b1509689a7160bbe7aa588f4075cc3e3
Omniprezz <geoffrey.frioli@gmail.com>
2018-04-29 14:54:33 +0200

    ExoJS Update

--- Calculatrice_exoJS#1/javascript.js
+++ Calculatrice_exoJS#1/javascript.js
@@ -9,4 +9,33 @@ Améliorer l'UI/UX pour que cela ressemble à une vraie calculatrice
 */
 
 
+function nombreUn() {
+	var nbr1 = prompt("Entrer un nombre");
+	return(nbr1);
+}
+
+function nombreDeux() {
+	var nbr2 = prompt("Entrer un nombre");
+	return(nbr2);
+}
+
+function operator() {
+	var opt = prompt("Entrer un operateur")
+	if (opt == "+") {
+		alert(nbr1 + nbr2);
+	} else if (opt == "-") {
+		alert(nbr1 - nbr2);
+	} else if (opt == "*") {
+		alert(nbr1 * nbr2);
+	} else if (opt == "/") {
+		alert(nbr1 / nbr2);
+	} else {
+		operator();
+	}
+
+}
+
+nombreUn();
+nombreDeux();
+operator();
 
