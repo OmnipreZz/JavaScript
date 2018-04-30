22b166e57a36eff697caca7ee6049ac3eb99bf59
Omniprezz <geoffrey.frioli@gmail.com>
2018-04-30 22:38:47 +0200

    Fonctionne

--- Calculatrice_exoJS#1/javascript.js
+++ Calculatrice_exoJS#1/javascript.js
@@ -1,41 +1,28 @@
-/*Calculatrice
-Réaliser un calculatrice simple
-Choisir une opération : + * / -
-Choisir un 1er nombre
-Choisir un 2eme nombre
-Afficher le résultat
-Bonus
-Améliorer l'UI/UX pour que cela ressemble à une vraie calculatrice
-*/
 
 
-function nombreUn() {
-	var nbr1 = prompt("Entrer un nombre");
-	return(nbr1);
-}
+function calcul() {
+  
+	var nbr1 = parseInt(document.getElementById('n1').value);
+    var nbr2 = parseInt(document.getElementById('n2').value);
 
-function nombreDeux() {
-	var nbr2 = prompt("Entrer un nombre");
-	return(nbr2);
-}
-
-function operator() {
-	var opt = prompt("Entrer un operateur")
-	if (opt == "+") {
-		alert(nbr1 + nbr2);
-	} else if (opt == "-") {
-		alert(nbr1 - nbr2);
-	} else if (opt == "*") {
-		alert(nbr1 * nbr2);
-	} else if (opt == "/") {
-		alert(nbr1 / nbr2);
-	} else {
-		operator();
-	}
+  if (document.getElementById('r1').checked) {
+    var addition = nbr1 + nbr2;
+    document.getElementById('afficher').innerHTML = addition;
+  } else if (document.getElementById('r2').checked){
+    var soustraction = nbr1 - nbr2;
+    document.getElementById('afficher').innerHTML = soustraction;
+  } else if (document.getElementById('r3').checked){
+    var multiplication = nbr1 * nbr2;
+    document.getElementById('afficher').innerHTML = multiplication;
+  } else if (document.getElementById('r4').checked){
+    var division = nbr1 / nbr2;
+    document.getElementById('afficher').innerHTML = addition;
+  } else {
+    document.getElementById('afficher').innerHTML = "je n'ai pas compris";
+  }
 
 }
 
-nombreUn();
-nombreDeux();
-operator();
+
+
 
